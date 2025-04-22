6b57c117ed0ff80b4f700c17049dff4a48e9a391
MKH-99x <minserve123@gmail.com>
2025-04-22 07:32:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function func1(event) {
+    event.preventDefault();
+    alert("Submission Successful...!!");
+    document.getElementById('in').value = "";
+    document.getElementById('in1').value = "";document.getElementById('in2').value = "";
+}
